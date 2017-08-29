f2360246d3f02878c3523c86395bfc3a9b3a6ad0
Alexander Campbell <acampbell2231@gmail.com>
2017-08-29 09:29:01 -0600

    Basic todo list

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,48 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    page = Page.new(page_params)
+    if page.save
+      redirect_to page_path(page)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(page_params)
+      redirect_to page_path(@page)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def destroy
+    page = Page.find(params[:id])
+    page.destroy
+    redirect_to pages_path
+  end
+
+
+  private
+  def page_params
+    params.require(:page).permit(:title, :author, :body, :private)
+  end
+
+end
